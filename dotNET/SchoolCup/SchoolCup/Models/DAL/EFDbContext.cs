cbb4ac23b31c5b0e6affe5c614b3d9e10aff19a6
Jonathan Heirbaut <jonathan.heirbaut.1@student.kdg.be>
2013-03-15 15:20:24 +0100

    .NET efdb

--- dotNET/SchoolCup/SchoolCup/Models/DAL/EFDbContext.cs
+++ dotNET/SchoolCup/SchoolCup/Models/DAL/EFDbContext.cs
@@ -4,9 +4,22 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
+using System.Data.Entity.ModelConfiguration.Conventions;
+
 namespace SchoolCup.Models.DAL
 {
   public class EFDbContext : DbContext 
   {
+
+    public GTDContext():base("Kdg_Schoolcup_Version01")
+    {}
+
+
+
+
+          protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    }
   }
 }
