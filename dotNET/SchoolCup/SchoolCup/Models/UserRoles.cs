44e10cc5ad6a2a4f8c52ebe0a874dcebae4b762c
Jonathan Heirbaut <jonathan.heirbaut.1@student.kdg.be>
2013-03-29 15:33:46 +0100

    .NET roles

---
+++ dotNET/SchoolCup/SchoolCup/Models/UserRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolCup.Models
+{
+  public enum UserRoles
+  {
+    Administrator = 6,
+    Nsf = 5,
+    Coordinator = 4,
+    Coach = 3,
+    Student = 2,
+    Visitor = 1
+  }
+}
