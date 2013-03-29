205e95d160287b371955f6161d1cc362d165d163
Thomas D'haen <thomasdhaen@me.com>
2013-03-29 13:34:23 +0100

    fixed bug active page, cleaned content folder

--- dotNET/SchoolCup/SchoolCup/Controllers/HomeController.cs
+++ dotNET/SchoolCup/SchoolCup/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SchoolCup.Models.DAL;
+using SchoolCup.Helpers;
 
 namespace SchoolCup.Controllers
 {
--- dotNET/SchoolCup/SchoolCup/Helpers/ActivePage.cs
+++ dotNET/SchoolCup/SchoolCup/Helpers/ActivePage.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 
-namespace SchoolCup
+namespace SchoolCup.Helpers
 {
     public static class ActivePage
     {
